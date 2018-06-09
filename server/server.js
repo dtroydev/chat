98d1cf6abb66dd3eeb98b9b4ced7f8e0e5c80190
Daniel <daniel@Daniels-Air.home.gateway>
2018-06-09 19:48:58 +1000

    basic socket.io

--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
 'use strict';
 
 const express = require('express');
-const path = require('path');
 
 const app = express();
-const staticPath = path.join(__dirname, '../public');
-const PORT = process.env.PORT || 3000;
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
+const staticPath = require('path').join(__dirname, '../public');
+
+const PORT = process.env.PORT || 3000;
 app.use(express.static(staticPath));
 
-app.listen(PORT, () => console.log(`Server started on ${PORT}`));
+server.listen(PORT, () => console.log(`Server started on ${PORT}`));
+
+io.on('connection', (socket) => {
+  console.log('User Connected');
+  socket.on('disconnect', () => {
+    console.log('User Disconnected');
+  });
+});
